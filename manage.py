5eb48af65b38496105f761423c687f74e3107794
Andrey Martyanov <andrey@martyanov.com>
2015-02-05 14:13:55 +0300

    A bit refactored manage commands.

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@
 """
 
 from flask.ext.migrate import MigrateCommand
-from flask.ext.script import Shell, Manager
+from flask.ext.script import Manager
 
 from notv import models
 from notv.app import create_app
@@ -17,15 +17,15 @@ from notv.extensions import db
 
 
 app = create_app()
+manager = Manager(app)
 
 
+@manager.shell
 def _make_context():
     return dict(app=app, db=db, models=models)
 
 
-manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-manager.add_command("shell", Shell(make_context=_make_context))
 
 
 if __name__ == '__main__':
