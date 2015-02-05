24b4a580c00e62309ac1989eb73814f93f69a8a2
Andrey Martyanov <andrey@martyanov.com>
2015-02-05 23:34:35 +0300

    Setup test infrastructure based on pytest and tox.

---
+++ tests/test_api.py
@@ -0,0 +1,26 @@
+"""
+    tests.test_api
+    ==============
+
+    API related tests.
+
+    :copyright: Copyright (c) 2015 Andrey Martyanov. All rights reserved.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import pytest
+
+from notv.app import create_app
+
+
+@pytest.fixture
+def client(request):
+    app = create_app()
+    app.config['TESTING'] = True
+    client = app.test_client()
+    return client
+
+def test_index_page(client):
+    r = client.get('/')
+    assert r.status_code == 200
+    assert r.data == b'Hello, World!'
