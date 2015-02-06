17faef93503d14aad31683a0719e2664ac408f7f
Andrey Martyanov <andrey@martyanov.com>
2015-02-07 00:02:57 +0300

    A bit refactored TVRage machinery and tests.

--- notv/feed.py
+++ notv/feed.py
@@ -27,48 +27,47 @@ class ParseError(Exception):
     pass
 
 
-class ShowEnded(Exception):
-    pass
+def _make_feed_url(show_id):
+    return BASE_FEED_URL + "episodeinfo.php?sid=" + str(show_id)
 
 
-class NextEpisodeNotFound(Exception):
-    pass
-
-
-def fetch_episode_data(show_id):
-    episode_url = BASE_FEED_URL + "episodeinfo.php?sid=" + str(show_id)
+def fetch_show_data(show_id):
+    feed_url = _make_feed_url(show_id)
     try:
-        return requests.get(episode_url, timeout=(5, 5)).content
-    except request.exception.RequestException as e:
+        return requests.get(feed_url, timeout=(5, 5)).content
+    except requests.exceptions.RequestException as e:
         logger.exception(e)
         raise FetchError(e)
 
 
-def parse_episode_data(episode_data):
-    episode = {}
+def parse_show_data(show_data):
+    show = {}
 
     try:
-        parsed = ET.fromstring(episode_data)
-    except (ValueError, TypeError, ET.ParseError):
+        parsed = ET.fromstring(show_data)
+    except (ValueError, TypeError, ET.ParseError) as e:
         logger.exception(e)
         raise ParseError(e)
 
-    if parsed.find('ended').text:
-        raise ShowEnded()
+    show['name'] = parsed.find('name').text
+    show['link'] = parsed.find('link').text
+
+    ended = parsed.find('ended').text
+    if ended:
+        show['ended'] = ended
 
     next_episode = parsed.find('nextepisode')
     if next_episode:
-        episode['next_episode_number'] = next_episode.find('number').text
-        episode['next_episode_title'] = next_episode.find('title').text
-        episode['next_episode_date'] = next_episode.find('airdate').text
-    else:
-        raise NextEpisodeNotFound()
+        show['next_episode_number'] = next_episode.find('number').text
+        show['next_episode_title'] = next_episode.find('title').text
+        show['next_episode_date'] = next_episode.find('airdate').text
 
     latest_episode = parsed.find('latestepisode')
-    episode['latest_episode_number'] = latest_episode.find('number').text
-    episode['latest_episode_title'] = latest_episode.find('title').text
-    episode['latest_episode_date'] = latest_episode.find('airdate').text
+    if latest_episode:
+        show['latest_episode_number'] = latest_episode.find('number').text
+        show['latest_episode_title'] = latest_episode.find('title').text
+        show['latest_episode_date'] = latest_episode.find('airdate').text
 
-    logger.debug("Succesuflly parsed episode data: {}".format(episode))
+    logger.debug("Succesuflly parsed episode data: {}".format(show))
 
-    return episode
+    return show
---
+++ tests/test_feed.py
@@ -0,0 +1,109 @@
+"""
+    tests.test_feed
+    ===============
+
+    Feed functionality related tests.
+
+    :copyright: Copyright (c) 2015 Andrey Martyanov. All rights reserved.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import pytest
+import responses
+
+from notv.feed import FetchError, ParseError, _make_feed_url, fetch_show_data, \
+    parse_show_data
+
+
+TEST_XML_DOCUMENT_SHOW_ON_AIR = b"""<show id='25'>
+    <name>Show name</name>
+    <link>http://www.tvrage.com/Show_name</link>
+    <ended></ended>
+    <latestepisode>
+        <number>04x02</number>
+        <title>Episode title</title>
+        <airdate>2015-02-02</airdate>
+    </latestepisode>
+    <nextepisode>
+        <number>04x03</number>
+        <title>Guilty</title>
+        <airdate>2015-02-09</airdate>
+    </nextepisode>
+</show>
+"""
+
+TEST_XML_DOCUMENT_SHOW_ENDED = b"""<show id='25'>
+    <name>Show name</name>
+    <link>http://www.tvrage.com/Show_name</link>
+    <ended>2015-02-02</ended>
+    <latestepisode>
+        <number>04x02</number>
+        <title>Episode title</title>
+        <airdate>2015-02-02</airdate>
+    </latestepisode>
+</show>
+"""
+
+
+def test_make_feed_url():
+    assert _make_feed_url(25) == ("http://services.tvrage.com/feeds/"
+                                  "episodeinfo.php?sid=25")
+
+
+@responses.activate
+def test_fetch_show_data_ok():
+    feed_url = _make_feed_url(25)
+    responses.add(responses.GET, feed_url,
+                  body=b'ok', status=200)
+
+    r = fetch_show_data(25)
+
+    assert r.content == b'ok'
+
+
+@responses.activate
+def test_fetch_show_data_error():
+    feed_url = _make_feed_url(25)
+    responses.add(responses.GET, feed_url,
+                  status=500)
+
+    with pytest.raises(FetchError):
+        fetch_show_data(25)
+
+
+def test_parse_show_data_with_invalid_input():
+    with pytest.raises(ParseError):
+        parse_show_data(None)
+
+    with pytest.raises(ParseError):
+        parse_show_data(5)
+
+    with pytest.raises(ParseError):
+        parse_show_data("bad data")
+
+
+def test_parse_show_data_when_show_on_air():
+    valid_keys = [
+        'name',
+        'link',
+        'next_episode_number',
+        'next_episode_title',
+        'next_episode_date',
+        'latest_episode_number',
+        'latest_episode_title',
+        'latest_episode_date',
+    ]
+    show = parse_show_data(TEST_XML_DOCUMENT_SHOW_ON_AIR)
+    assert all([key in show for key in valid_keys])
+    assert 'ended' not in show
+
+
+def test_parse_show_data_when_show_ended():
+    wrong_keys = [
+        'next_episode_number',
+        'next_episode_title',
+        'next_episode_date'
+    ]
+    show = parse_show_data(TEST_XML_DOCUMENT_SHOW_ENDED)
+    assert all([key not in show for key in wrong_keys])
+    assert 'ended' in show
